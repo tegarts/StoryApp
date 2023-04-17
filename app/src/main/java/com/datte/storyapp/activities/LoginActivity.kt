2e3f8903409842596b91b058db229138157f2665
Tegar Nur Hidayat <tegarnurhidayat4@gmail.com>
2023-04-17 10:52:51 +0700

    Initial push
    initial push

---
+++ app/src/main/java/com/datte/storyapp/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.datte.storyapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.datte.storyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
